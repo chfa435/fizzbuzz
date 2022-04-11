6ae6785036dac1028ee2bd37f041db23d5da37fd
chfa435 <79060477+chfa435@users.noreply.github.com>
2022-04-11 18:01:21 -0400

    first array

--- js/site.js
+++ js/site.js
@@ -1,27 +1,83 @@
- 
  //Step ONE (GET = controller, accepts a request)
-function getMessage(){
+ //First get the fizz and the buzz values from the user
+ function getValues() {
+     let fizzValue = document.getElementById("fizzValue").value;
+     let buzzValue = document.getElementById("buzzValue").value;
 
-    let msg = document.getElementById("txtMessage").value;
+     //check if the user entered numbers
+     fizzValue = parseInt(fizzValue);
+     buzzValue = parseInt(buzzValue);
 
-    displayMessage(msg);
+     if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+         //Call display values and pass fizz and buzz to the function
+         //calculate fizzbuzz
+         let fbValues = fizzBuzzA(fizzValue, buzzValue);
+         
+         //display the values
+         displayValues(fbValues);
 
+     } else {
+         alert("you must enter integers!");
+     }
+
+ }
+
+//business logic layer (LOGIC LAYER - Model)
+
+function fizzBuzzA(fizz, buzz) {
+
+    //create an array
+    let fbValues = [];
+
+    for (let i = 1; i <= 100; i++ ) {
+
+        if (i % fizz == 0 && i % buzz == 0) {
+            fbValues.push('FizzBuzz');
+        } else if (i % fizz == 0 ) {
+            fbValues.push('Fizz');
+        } else if (i % buzz == 0 ) {
+            fbValues.push('Buzz');
+        } else {
+            fbValues.push(i);
+        }
+    }
+
+    return fbValues;
 }
 
-//Final Step (DISPLAY = view)
-function displayMessage(message) {
-     
-    //first get the ol element from the page
-    element = document.getElementById("results");
-    
-    //next create a new li element
-    let item = document.createElement("li");
-    //add classes to the li element
-    item.classList.add("list-group-item");
-    //set the message for the li element
-    item.innerHTML = message;
 
-    //add the new item to the list
-    element.appendChild(item);
-}
+ //Final Step (DISPLAY = view)
+ function displayValues(fbValues) {
+     //Fizz = 3
+     //Buzz = 5
 
+     //display a list of numbers from 1 to 100
+
+
+     //get the results div from teh page
+     let element = document.getElementById("results");
+
+     //reset the display
+     element.innerHTML = "";
+
+     for(let index = 0; index < fbValues.length; index++){
+
+        let item = document.createElement("div");
+        item.classList.add("col");
+
+        item.innerHTML = fbValues[index];
+
+        if (fbValues[index] == 'Fizz') {
+            item.classList.add("Fizz");
+        }
+        else if (fbValues[index] == 'Buzz') {
+            item.classList.add("Buzz");
+        }
+        else if (fbValues[index] == 'FizzBuzz') {
+            item.classList.add("FizzBuzz");
+        }
+
+        element.appendChild(item);
+
+     }
+    }
